c6ce97c717810cc00c1bc964029cb26aedc5c1aa
Mariano Shaar <a00825287@itesm.mx>
2020-06-26 18:33:35 -0500

    Add sendToRandomVideo function and remove trailing whitespace

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -11,3 +11,22 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+/**
+ * Sends the user to a random Youtube video on a different tab
+ */
+function sendToRandomVideo() {
+  const videos = [
+    "https://www.youtube.com/watch?v=Ng_Im-qsWzc&list=LLGpjNAdp_solk9m7e4SaPsg&index=8",
+    "https://www.youtube.com/watch?v=sYd_-pAfbBw&list=LLGpjNAdp_solk9m7e4SaPsg&index=48",
+    "https://www.youtube.com/watch?v=xuCn8ux2gbs&list=LLGpjNAdp_solk9m7e4SaPsg&index=61",
+    "https://www.youtube.com/watch?v=huEtJw7pfLk&list=LLGpjNAdp_solk9m7e4SaPsg&index=68",
+    "https://www.youtube.com/watch?v=Uu5zGHjRaMo&list=LLGpjNAdp_solk9m7e4SaPsg&index=22",
+  ];
+
+  const randomVideo = videos[Math.floor(Math.random() * videos.length)];
+
+  document.getElementById('video-button').onclick = function() {
+    document.getElementById("link_b").setAttribute("href", "https://www.google.com");
+  }
+}
