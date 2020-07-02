adda91d8d05e8b3db5ef3361428df5f817107e41
Mariano Shaar <a00825287@itesm.mx>
2020-07-02 17:13:48 +0000

    Remove greeting from doGet

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -34,8 +34,7 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("Hello stranger! Welcome to my portfolio");
+    
   }
 
   private String convertToJsonUsingGson(TestCommentObject commentList) {
