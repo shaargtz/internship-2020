eae32c0eb1227d2837dec9caea0eb0f7c0b790a8
Mariano Shaar <a00825287@itesm.mx>
2020-07-02 17:12:18 +0000

    Remove incomplete for loop

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -34,7 +34,6 @@ public class DataServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    for ()
     response.setContentType("text/html;");
     response.getWriter().println("Hello stranger! Welcome to my portfolio");
   }
