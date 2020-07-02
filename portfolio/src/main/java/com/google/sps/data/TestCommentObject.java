129120637f560199aa6d96103926db1b6ed15daa
Mariano Shaar <a00825287@itesm.mx>
2020-07-02 16:57:55 +0000

    Add test comment class file

---
+++ portfolio/src/main/java/com/google/sps/data/TestCommentObject.java
@@ -0,0 +1,10 @@
+package com.google.sps.servlets;
+
+public final class TestCommentObject {
+
+  private final ArrayList<String> commentList;
+
+  public ServerStats(ArrayList<String> commentList) {
+    this.commentList = commentList;
+  }
+}
