1126fb96d17f944ee9f23d7140a92f008471086b
Mariano Shaar <a00825287@itesm.mx>
2020-07-03 23:16:18 +0000

    Reorder imports as by the styleguide

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,14 +14,14 @@
 
 package com.google.sps.servlets;
 
-import java.util.ArrayList;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.gson.Gson;
 import com.google.sps.servlets.CommentList;
 import com.google.sps.servlets.Comment;
 import java.io.IOException;
-import com.google.gson.Gson;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
