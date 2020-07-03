1b8083016a99ed762126b9ffaa8d121bd3cd8f9a
Mariano Shaar <a00825287@itesm.mx>
2020-07-03 19:36:33 +0000

    Fix the definition of commentList inside of the class

--- portfolio/src/main/java/com/google/sps/data/TestCommentObject.java
+++ portfolio/src/main/java/com/google/sps/data/TestCommentObject.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public final class TestCommentObject {
 
-  private final ArrayList<String> commentList;
+  private final ArrayList<String> commentList = new ArrayList<String>();
 
   public void addComment(String comment) {
     commentList.add(comment);
