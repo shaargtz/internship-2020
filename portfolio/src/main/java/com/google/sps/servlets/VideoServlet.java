8c79a0c0b5f9d6863593768f839c93ec94556580
Mariano Shaar <a00825287@itesm.mx>
2020-07-06 20:55:15 +0000

    Write doGet function to retrieve the index of the last video that was played, to not repeat it

--- portfolio/src/main/java/com/google/sps/servlets/VideoServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/VideoServlet.java
@@ -2,4 +2,17 @@
 @WebServlet("/video")
 public class VideoServlet extends HttpServlet {
 
+  public int lastVideoIndex = 4;
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String json = "{";
+    json += "\"lastVideoIndex\": ";
+    json += "\"" + lastVideoIndex + "\"";
+    json += "}";
+    
+    // Send the JSON as the response.
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
 }
