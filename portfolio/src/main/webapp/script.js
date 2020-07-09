f9d3336ac8385b63917753042e244a722154f065
Mariano Shaar <a00825287@itesm.mx>
2020-07-09 19:37:28 +0000

    Change center location of map

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -25,8 +25,10 @@ function onBodyLoad() {
  */
 function createMap() {
   const map = new google.maps.Map(
-      document.getElementById('map'),
-      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+      document.getElementById('map'), {
+        center: {lat: 25.650955, lng: -100.289508},
+        zoom: 16,
+      });
 }
 
 /**
