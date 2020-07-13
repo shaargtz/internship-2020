369c22f7f920f2d00e24f07724b5889140b0411c
Mariano Shaar <a00825287@itesm.mx>
2020-07-13 20:57:37 +0000

    Add chart-data servlet to parse csv data

---
+++ portfolio/src/main/java/com/google/sps/servlets/ChartServlet.java
@@ -0,0 +1,13 @@
+package com.google.sps.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/** Servlet that gets data from a csv and parses it as to use it in a chart.*/
+@WebServlet("/chart-data")
+public class VideoServlet extends HttpServlet {
+  
+}
