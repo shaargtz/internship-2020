15f90ce7f608f958728a7049e929a0902d1fa82c
Mariano Shaar <a00825287@itesm.mx>
2020-07-14 17:36:58 +0000

    Make Javadoc more specific

--- portfolio/src/main/java/com/google/sps/servlets/ChartServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ChartServlet.java
@@ -9,12 +9,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that gets data from a csv and parses it as to use it in a chart.*/
+/** Servlet that gets data from a csv and parses it to display in a chart.*/
 @WebServlet("/chart-data")
 public class ChartServlet extends HttpServlet {
+
+  // Ordered map to store the pairs of values parsed from the csv.
   private LinkedHashMap<String, Integer> memeSearchTrend = new LinkedHashMap<>();
 
-  /** Parses csv data a single time instead of on every request.*/
+  /**
+   * Parses csv data a single time instead of on every request.
+   * The file should be in the format date, interest level.
+   * Date must be in any format that makes it a valid string for the
+   * Date() constructor, and at least have year and month.
+   * Interest level must be an integer.
+   */
   @Override
   public void init() {
     Scanner scanner = new Scanner(getServletContext().getResourceAsStream(
@@ -22,16 +30,17 @@ public class ChartServlet extends HttpServlet {
     while (scanner.hasNextLine()) {
       String line = scanner.nextLine();
       String[] cells = line.split(",");
-
       String date = cells[0];
       Integer interest = Integer.valueOf(cells[1]);
-
       memeSearchTrend.put(date, interest);
     }
     scanner.close();
   }
 
-  /** Sends parsed data in JSON format.*/
+  /** 
+   * Sends key value pairs in year:interest format, parsed from the csv,
+   * converted to JSON.
+   */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     response.setContentType("application/json");
